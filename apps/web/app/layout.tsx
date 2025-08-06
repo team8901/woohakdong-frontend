36cd09141b344f73a1b67a2d345b194805decd8d
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2025-08-06 22:48:15 +0900

    Init: Create project
    Turborepo를 이용한 모노레포 생성
    - Next.js, TypeSciprt, Shadcn/ui, Tailwind CSS 적용

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,30 @@
+import '@workspace/ui/globals.css';
+import { Providers } from '@/components/providers';
+import localFont from 'next/font/local';
+import type { Metadata } from 'next';
+
+const pretendard = localFont({
+  src: '../../../packages/ui/src/fonts/pretendard/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '100 900',
+  variable: '--font-pretendard',
+});
+
+export const metadata: Metadata = {
+  title: '우학동',
+  description: '우학동 : 귀찮았던 동아리 관리, 우학동이 대신 해 드릴게요',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko" suppressHydrationWarning>
+      <body className={`${pretendard.variable} antialiased`}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
