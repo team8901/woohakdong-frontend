4ea976c8b2334e12411cd16d610eea0dda77b069
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2025-08-12 16:51:27 +0900

    Design: Update login form ui
    로그인 폼 UI 디자인 수정

--- apps/web/src/app/login/_clientBoundary/login-form.tsx
+++ apps/web/src/app/login/_clientBoundary/login-form.tsx
@@ -23,38 +23,30 @@ const LoginForm = ({ className, ...props }: React.ComponentProps<'div'>) => {
 
   return (
     <div className={cn('flex flex-col gap-6', className)} {...props}>
-      <div className="flex flex-col gap-6">
-        <div className="flex flex-col items-center gap-6">
-          <a
-            href="https://landing.woohakdong.com/"
-            className="font-jua text-primary flex items-center gap-2 self-center text-xl transition-all hover:scale-110">
-            우학동
-          </a>
-          <div className="flex flex-col items-center gap-2 text-center">
-            <h1 className="text-2xl font-bold">로그인</h1>
-            <p className="text-muted-foreground text-balance font-semibold">
-              학교 구글 계정으로 간편하게 시작해보세요!
-            </p>
-          </div>
-        </div>
-        <Button
-          variant="outline"
-          type="button"
-          className="w-full"
-          onClick={handleGoogleLogin}>
-          <GoogleIcon />
-          Google로 시작하기
-        </Button>
-        <div className="text-muted-foreground *:[a]:underline *:[a]:underline-offset-4 text-balance text-center text-sm">
-          <a href="#">서비스 이용약관</a> 및 <a href="#">개인정보 처리방침</a>
-        </div>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">로그인</h1>
+        <p className="text-muted-foreground text-balance">
+          학교 구글 계정으로 간편하게 시작해보세요!
+        </p>
       </div>
-      <div className="after:border-border relative after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t"></div>
-      <div className="text-muted-foreground text-center text-sm">
+      <Button
+        variant="outline"
+        type="button"
+        className="w-full"
+        onClick={handleGoogleLogin}>
+        <GoogleIcon />
+        Google로 시작하기
+      </Button>
+      <div className="text-center text-sm">
         도움이 필요하신가요?{' '}
-        <a href="#" className="text-foreground font-semibold">
+        <a href="#" className="font-semibold">
           도움받기
         </a>
+      </div>
+      <div className="after:border-border relative after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t"></div>
+      <div className="text-muted-foreground *:[a]:underline *:[a]:underline-offset-4 text-balance text-center text-xs">
+        로그인 시 <a href="#">이용약관</a>과 <a href="#">개인정보 처리방침</a>에
+        동의한 것으로 간주됩니다.
       </div>
     </div>
   );
--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -5,6 +5,11 @@ import LoginForm from './_clientBoundary/login-form';
 const LoginPage = () => {
   return (
     <div className="bg-background flex min-h-screen w-screen flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <a
+        href="https://landing.woohakdong.com/"
+        className="font-jua text-primary flex items-center gap-2 self-center text-xl transition-all hover:scale-110">
+        우학동
+      </a>
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
