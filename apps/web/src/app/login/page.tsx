6c1a025d6c902acd156c1d591252c149ba1a8a64
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2025-08-13 23:59:18 +0900

    Feat: Implement sign up page ui
    회원가입 폼 UI 구현

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -5,7 +5,7 @@ const LoginPage = () => {
     <div className="bg-background flex min-h-screen w-screen flex-col items-center justify-center gap-6 p-6 md:p-10">
       <a
         href="https://landing.woohakdong.com/"
-        className="font-jua text-primary flex items-center gap-2 self-center text-xl transition-all hover:scale-110">
+        className="font-jua text-primary text-xl transition-all hover:scale-110">
         우학동
       </a>
       <div className="w-full max-w-sm">
