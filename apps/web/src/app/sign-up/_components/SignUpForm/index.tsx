28431d5e300a83928210cbe7d142ee60d966b110
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2025-08-14 17:07:32 +0900

    Refactor: Refactoring sign-up page
    회원가입 페이지의 코드들을 리팩토링
    - 회원가입 UI 카드를 클라이언트 컴포넌트로 분리
    - 카드 내의 입력 폼을 컴포넌트로 분리
    - 입력된 정보를 제출하기 위한 로직도 커스텀 훅으로 분리
    - 유효성 검사에 사용되는 Zod 스키마 분리

---
+++ apps/web/src/app/sign-up/_components/SignUpForm/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { UseFormReturn } from 'react-hook-form';
+import { Form } from '@workspace/ui/components/form';
+import { FormData } from '../../_helpers/utils/zodSchemas';
+import { UserInfoForm } from '../UserInfoForm';
+import { UserSchoolInfoForm } from '../UserSchoolInfoForm';
+
+type SignUpFormProps = {
+  form: UseFormReturn<FormData>;
+  step: 1 | 2;
+  onSubmit: (data: FormData) => Promise<void>;
+};
+
+export const SignUpForm = ({ form, step, onSubmit }: SignUpFormProps) => {
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        {step === 1 && <UserInfoForm form={form} />}
+        {step === 2 && <UserSchoolInfoForm form={form} />}
+      </form>
+    </Form>
+  );
+};
