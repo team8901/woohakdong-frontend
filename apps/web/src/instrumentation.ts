7c74372a7cbdadc5b752f66279816955bd99cba4
salmonco <xkqjsslsek80@naver.com>
2025-08-18 18:06:29 +0900

    Chore: Add comment

--- apps/web/src/instrumentation.ts
+++ apps/web/src/instrumentation.ts
@@ -1,6 +1,10 @@
 import { IS_MOCK } from '@/mock/config/env';
 
 export async function register() {
+  /**
+   * 서버 컴포넌트나 api 라우트 등이 node.js 환경에서 실행될 때에만 처리
+   * @see https://nextjs.org/docs/app/guides/instrumentation#importing-runtime-specific-code
+   */
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     if (IS_MOCK) {
       const { mockServerListen } = await import('./mock/server');
