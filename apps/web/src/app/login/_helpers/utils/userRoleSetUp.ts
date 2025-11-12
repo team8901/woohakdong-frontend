336ab316aefc8bb1db0ed39a9a7e775073d9f9d3
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2025-11-12 17:06:16 +0900

    Refactor: Improve middleware logic and separate user role API
    - 미들웨어의 권한별 페이지 접근 제어 로직 변경(권한 상관없이 club-list 접근, 정회원 sign-up 페이지 접근 제한)
    - 유저 권한 설정 API 호출을 별도 함수로 분리

--- apps/web/src/app/login/_helpers/utils/userRoleSetUp.ts
+++ apps/web/src/app/login/_helpers/utils/userRoleSetUp.ts
@@ -1,45 +1,31 @@
 import { getMyProfile } from '@/data/user/getMyProfile/fetch';
+import { postUserRoleAssociate } from '@/data/user/postUserRoleAssociate/post';
+import { putUserRoleRegular } from '@/data/user/putUserRoleRegular/put';
 import { isAxiosError } from '@workspace/api/axios';
 
 /**
  * 사용자 프로필 정보를 확인하고 적절한 페이지로 리다이렉트하는 유틸리티 함수
  * @returns Promise<void>
  */
-
 export const setUserRoleAndRefresh = async (): Promise<void> => {
   try {
     await getMyProfile();
 
-    console.log('✅ 프로필 정보 조회 성공 - 정회원');
+    console.log('✅ 프로필 정보 조회 성공');
 
     // 유저 권한(정회원) 쿠키 설정
-    try {
-      await fetch('/api/auth/roles', {
-        method: 'PUT',
-      });
+    await putUserRoleRegular();
 
-      console.log('✅ 유저 권한(정회원) 쿠키 설정 완료');
-    } catch (error) {
-      console.error('❌ 유저 권한(정회원) 등록 중 에러가 발생했습니다.', error);
-    }
+    console.log('✅ 유저 권한(정회원) 쿠키 설정 완료');
   } catch (err) {
     if (isAxiosError(err)) {
       if (err.response?.status === 404) {
-        console.log('⚠️ 프로필 정보 없음 - 준회원');
+        console.log('⚠️ 프로필 정보 없음');
 
         // 유저 권한(준회원) 쿠키 설정
-        try {
-          await fetch('/api/auth/roles', {
-            method: 'POST',
-          });
+        await postUserRoleAssociate();
 
-          console.log('✅ 유저 권한(준회원) 쿠키 설정 완료');
-        } catch (error) {
-          console.error(
-            '❌ 유저 권한(준회원) 등록 중 에러가 발생했습니다.',
-            error,
-          );
-        }
+        console.log('✅ 유저 권한(준회원) 쿠키 설정 완료');
       }
     } else {
       throw err;
