816d6939ef6aec750b4b123a19e95bb5749c8158
salmonco <xkqjsslsek80@naver.com>
2025-11-19 15:38:06 +0900

    Feat: Scroll table

--- apps/web/src/app/(dashboard)/member/_components/MemberTable/index.tsx
+++ apps/web/src/app/(dashboard)/member/_components/MemberTable/index.tsx
@@ -9,81 +9,83 @@ type Props = {
 
 export const MemberTable = ({ members }: Props) => {
   return (
-    <table className="min-w-full divide-y divide-gray-200">
-      <thead className="bg-gray-50">
-        <tr>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
-            이름
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
-            역할
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
-            성별
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
-            전화번호
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
-            이메일
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
-            학과
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
-            학번
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
-            가입일
-          </th>
-        </tr>
-      </thead>
-      <tbody className="divide-y divide-gray-200 bg-white">
-        {members.map((member) => (
-          <tr key={member.id}>
-            <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
-              {member.name}
-            </td>
-            <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
-              {getKeyByValue(CLUB_MEMBER_ROLE, member.role)}
-            </td>
-            <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
-              {getKeyByValue(CLUB_MEMBER_GENDER, member.gender)}
-            </td>
-            <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
-              {member.phoneNumber}
-            </td>
-            <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
-              {member.email}
-            </td>
-            <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
-              {member.major}
-            </td>
-            <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
-              {member.studentNumber}
-            </td>
-            <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
-              {member.joinedDate}
-            </td>
+    <div className="overflow-x-auto">
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
+              이름
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
+              역할
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
+              성별
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
+              전화번호
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
+              이메일
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
+              학과
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
+              학번
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-900">
+              가입일
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="divide-y divide-gray-200 bg-white">
+          {members.map((member) => (
+            <tr key={member.id}>
+              <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                {member.name}
+              </td>
+              <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                {getKeyByValue(CLUB_MEMBER_ROLE, member.role)}
+              </td>
+              <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                {getKeyByValue(CLUB_MEMBER_GENDER, member.gender)}
+              </td>
+              <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                {member.phoneNumber}
+              </td>
+              <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                {member.email}
+              </td>
+              <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                {member.major}
+              </td>
+              <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                {member.studentNumber}
+              </td>
+              <td className="whitespace-nowrap px-6 py-4 text-sm text-gray-500">
+                {member.joinedDate}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
