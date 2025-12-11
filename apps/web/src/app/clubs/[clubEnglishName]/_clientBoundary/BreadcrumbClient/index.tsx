b81c50e597b9961d6463bb7a317a3fd9851de413
salmonco <xkqjsslsek80@naver.com>
2025-12-11 11:30:44 +0900

    Feat: Update breadcrumb

--- apps/web/src/app/clubs/[clubEnglishName]/_clientBoundary/BreadcrumbClient/index.tsx
+++ apps/web/src/app/clubs/[clubEnglishName]/_clientBoundary/BreadcrumbClient/index.tsx
@@ -1,24 +1,38 @@
 'use client';
 
+import { APP_PATH } from '@/_shared/helpers/constants/appPath';
+import { buildUrlWithParams } from '@/_shared/helpers/utils/buildUrlWithParams';
 import {
   Breadcrumb,
   BreadcrumbItem,
+  BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from '@workspace/ui/components/breadcrumb';
-import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { useParams, usePathname } from 'next/navigation';
 
 import { getPathData } from '../../_helpers/utils/dashboardHeaderUtils';
 
 export const BreadcrumbClient = () => {
   const pathname = usePathname();
+  const params = useParams<{ clubEnglishName: string }>();
   const headerData = getPathData(pathname);
+
+  const homeUrl = buildUrlWithParams({
+    url: APP_PATH.CLUBS.HOME,
+    pathParams: { clubEnglishName: params.clubEnglishName },
+  });
 
   return (
     <Breadcrumb>
       <BreadcrumbList>
-        <BreadcrumbItem className="block">{headerData.category}</BreadcrumbItem>
+        <BreadcrumbItem className="block">
+          <BreadcrumbLink asChild>
+            <Link href={homeUrl}>{headerData.category}</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
         <BreadcrumbSeparator className="block" />
         <BreadcrumbItem>
           <BreadcrumbPage>{headerData.title}</BreadcrumbPage>
