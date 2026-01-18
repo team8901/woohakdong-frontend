cfd9f9c436c36518ad38d98758cf6bebaf50a14b
salmonco <xkqjsslsek80@naver.com>
2026-01-18 18:57:26 +0900

    Feat(landing): delete github url

--- apps/landing/src/app/_helpers/constants/service.ts
+++ apps/landing/src/app/_helpers/constants/service.ts
@@ -2,8 +2,6 @@ export const SERVICE_NAME = '우학동';
 export const SERVICE_FULL_NAME = '우학동 : 우리 학교 동아리';
 export const SERVICE_DESCRIPTION = '우학동 랜딩 페이지';
 
-export const GITHUB_URL = 'https://github.com/team8901';
-
 // 사업자 정보
 export const BUSINESS_INFO = {
   companyName: '살몬코(salmonco)',
@@ -13,4 +11,3 @@ export const BUSINESS_INFO = {
   address: '경기도 성남시 분당구 내정로113번길 9 (정자동) 104호',
   phoneNumber: '070-8065-4280',
 } as const;
-
